b697f6faaf7f5838e08efae0807ba2865ff2b74f
Dustin L. Dodson <dustinleedodson@gmail.com>
2020-07-27 09:54:48 -0700

    Added customization options such as name, function context, logger, etc.

--- index.js
+++ index.js
@@ -2,20 +2,19 @@
 
 const promiseOrNot = require('@fizzygalacticus/promise-or-not');
 const { getLogger } = require('@fizzygalacticus/colored-fancy-log');
-const logger = getLogger({ name: 'trythis' });
 
-const printError = err => {
-    if (err.message) {
-        logger.error(err.message);
-    } else {
-        logger.error(err);
-    }
-};
+module.exports = (fn = () => {}, { name = 'trythis', context, logger = getLogger({ name }) } = {}) => (...params) => {
+    const printError = err => {
+        if (err.message) {
+            logger.error(err.message);
+        } else {
+            logger.error(err);
+        }
+    };
 
-const printSuccess = result => logger.success('received:', result);
+    const printSuccess = result => logger.success('received:', result);
 
-module.exports = (fn = () => {}) => (...params) => {
-    logger.info('trying...');
+    (logger.info || logger.log)('trying...');
 
-    return promiseOrNot(fn, printSuccess, printError)(...params);
+    return promiseOrNot(fn, { context, onData: printSuccess, onError: printError })(...params);
 };
